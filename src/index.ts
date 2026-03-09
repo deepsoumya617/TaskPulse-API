2f79752aac9c0d84fc62ff77637ac9639b2a1207
Soumyadeep Ghosh <soumyadeepghosh617@gmail.com>
2026-03-09 19:11:10 +0530

    added hostname 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import app from './app';
 // Load environment variables from .env file
 dotenv.config();
 
-const port = process.env.PORT || 8080;
+const port = parseInt(process.env.PORT || '8080', 10);
 // const mongoUri = process.env.MONGO_LOCAL_URI!
 const mongoCloudUri = process.env.MONGO_CLOUD_URI!;
 
@@ -16,6 +16,6 @@ mongoose
   .catch(err => console.error('MongoDB connection error:', err));
 
 // run the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
